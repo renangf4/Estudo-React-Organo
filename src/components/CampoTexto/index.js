a5d4cabacd446bbe4a1e455e94705589e3647a6a
Renan Morais <renanmorais@live.com>
2024-05-29 23:44:44 -0300

    iniciado o componente campo de texto

---
+++ src/components/CampoTexto/index.js
@@ -0,0 +1,13 @@
+import './CampoTexto.css';
+
+const CampoTexto = (props) => {
+    console.log(props);
+    return(
+        <div className="campo-texto">
+            <label>Nome</label>
+            <input placeholder="Digite o seu nome"/>
+        </div>
+    )
+}
+
+export default CampoTexto;
