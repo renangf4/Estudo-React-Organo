e033b14fda4a47a8a007ae33906ab350c0f9cf37
Renan Morais <renanmorais@live.com>
2024-09-22 00:33:57 -0300

    criado comportamento do formulario para criar objetos dos cards

--- src/components/Formulario/index.js
+++ src/components/Formulario/index.js
@@ -4,7 +4,7 @@ import CampoTexto from '../CampoTexto';
 import ListaSuspensa from '../ListaSuspensa';
 import './Formulario.css';
 
-const Formulario = () => {
+const Formulario = (props) => {
     const times = [
         "Programação",
         "Front-End",
@@ -22,7 +22,13 @@ const Formulario = () => {
 
     const aoSalvar = (evento) => {
         evento.preventDefault();
-        console.log('form submit', nome, cargo, imagem,time);
+
+        props.aoColaboradorCadastrado({
+            nome,
+            cargo,
+            imagem,
+            time
+        })
     }
 
     return (
