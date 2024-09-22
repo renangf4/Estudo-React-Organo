be14e1dc7e9dedcb5d2dbaff51c57fb5df49994e
Renan Morais <renanmorais@live.com>
2024-09-22 00:58:44 -0300

    iniciado criacao da secao time como objeto passando as cores

--- src/App.js
+++ src/App.js
@@ -1,8 +1,42 @@
 import { useState } from 'react';
 import Banner from './components/Banner';
 import Formulario from './components/Formulario';
+import Time from './components/Time';
 
 function App() {
+  const times = [
+    {
+      nome: 'Front-End',
+      corPrimaria: '#82CFFA',
+      corSecundaria: '#E8F8FF',
+    },
+    {
+        nome: 'Data Sciense',
+        corPrimaria: '#A6D157',
+        corSecundaria: '#F0F8E2',
+    },
+    {
+        nome: 'Devops',
+        corPrimaria: '#E06B69',
+        corSecundaria: '#FDE7E8',
+    },
+    {
+        nome: 'UX e Design',
+        corPrimaria: '#D86EBF',
+        corSecundaria: '#FAE5F5',
+    },
+    {
+        nome: 'Mobile',
+        corPrimaria: '#FEBA05',
+        corSecundaria: '#FFF5D9',
+    },
+    {
+        nome: 'Inovação e Gestão',
+        corPrimaria: '#FF8A29',
+        corSecundaria: '#FFEEDF',
+    }
+  ]
+
   const [colaboradores, setColaboradores] = useState([]);
 
   const aoNovoColaboradorAdicionado = (colaborador) => {
@@ -14,6 +48,7 @@ function App() {
     <div className="App">
       <Banner />
       <Formulario aoColaboradorCadastrado={colaborador => aoNovoColaboradorAdicionado(colaborador)}/>
+      {times.map(time => <Time key={time.nome} nome={time.nome} corPrimaria={time.corPrimaria} corSecundaria={time.corSecundaria}/>)}
     </div>
   );
 }
