80fe7f13438a6b23581614b3876486538de4ebf2
Renan Morais <renanmorais@live.com>
2024-09-22 01:38:29 -0300

    finalizado

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -2,8 +2,11 @@ import './Banner.css';
 
 export const  Banner = () => {
     return (
-        <header className="banner">
-            <img src="/imagens/banner.png" alt="Banner principal da página do Organo"/>
-        </header>
+        <>
+            <header className="banner">
+                <img src="/imagens/banner.png" alt="Banner principal da página do Organo"/>
+            </header>
+            <h1>Pessoas e times</h1>
+        </>
     )
 }
